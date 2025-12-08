09d5ab2e7bf65ee6b2f2fd094b5c4c7b73573ceb
tuanvmhe176448 <tuanvmhe176448.fpt.edu.vn>
2025-12-08 08:07:37 +0700

    update controller

---
+++ src/java/controller/trainee/TakeTestController.java
@@ -0,0 +1,38 @@
+package controller.trainee;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author vomin
+ */
+
+import dao.testDao;
+import model.Question;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/trainee/taketest")
+public class TakeTestController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        int testId = Integer.parseInt(req.getParameter("testId"));
+
+        testDao dao = new testDao();
+        List<Question> questions = dao.getQuestionsByTest(testId);
+
+        req.setAttribute("questions", questions);
+        req.setAttribute("testId", testId);
+
+        req.getRequestDispatcher("/View/Trainee/TakeTest.jsp").forward(req, resp);
+    }
+}
