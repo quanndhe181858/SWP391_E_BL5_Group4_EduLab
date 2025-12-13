71bef7fc24a90d6ba64a5f37821bc4638070eade
haohoangdanh1 <haohoangdanh1@gmail.com>
2025-12-13 13:27:19 +0700

    Add email sender for send mail

--- src/java/util/Email.java
+++ src/java/util/Email.java
@@ -4,10 +4,90 @@
  */
 package util;
 
+import java.util.Date;
+import java.util.Properties;
+
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
+import java.util.ResourceBundle;
+
 /**
  *
  * @author hao
  */
 public class Email {
-    
+
+    static final ResourceBundle bundle = ResourceBundle.getBundle("configuration.google");
+    static final String EMAIL_FROM = bundle.getString("EMAIL_FROM");
+    static final String EMAIL_PASSWORD = bundle.getString("EMAIL_PASSWORD");
+
+    public static boolean sendEmail(String to, String subject, String content) {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com"); // SMTP HOST
+        props.put("mail.smtp.port", "587"); // TLS 587 SSL 465
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Authenticator auth = new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(EMAIL_FROM, EMAIL_PASSWORD);
+            }
+        };
+
+        // Tạo phiên làm việc
+        Session session = Session.getInstance(props, auth);
+
+        // Tạo một tin nhắn
+        MimeMessage msg = new MimeMessage(session);
+
+        try {
+            // Kiểu nội dung
+            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
+
+            // Người gửi
+            msg.setFrom(new InternetAddress(EMAIL_FROM));
+
+            // Người nhận
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+
+            // Tiêu đề email
+            msg.setSubject(subject);
+
+            // Ngày gửi
+            msg.setSentDate(new Date());
+
+            // Nội dung
+            msg.setContent(content, "text/HTML; charset=UTF-8");
+
+            // Gửi email
+            Transport.send(msg);
+            System.out.println("Gửi email thành công");
+            return true;
+        } catch (MessagingException e) {
+            // Chỉ in thông điệp lỗi
+            System.err.println("Gặp lỗi trong quá trình gửi email: " + e.getMessage());
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 1; i++) {
+            boolean result = sendEmail("quanngo20311@gmail.com",
+                    "Lêu lêu FA " + System.currentTimeMillis(),
+                    "Đây là phần nội dung trêu mấy đứa FA " + System.currentTimeMillis());
+            if (result) {
+                System.out.println("Email " + (i + 1) + " đã được gửi thành công.");
+            } else {
+                System.out.println("Gửi email " + (i + 1) + " không thành công.");
+            }
+        }
+    }
+
 }
