16ac4e3bb55fec582693178b10b6c618a208bb9a
tuanvm <tuanvmhe176448@fpt.edu.vn>
2025-12-17 23:26:00 +0700

    iter3 - manager certificate

--- src/java/dtos/CertificateDTO.java
+++ src/java/dtos/CertificateDTO.java
@@ -2,39 +2,39 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package model;
+package dtos;
 
 import java.sql.Timestamp;
 
-/**
- *
- * @author vomin
- */
-public class Certificate {
+public class CertificateDTO {
 
     private int id;
     private String title;
     private int courseId;
+    private String courseTitle;
     private Integer categoryId;
+    private String categoryName;
     private String description;
     private String codePrefix;
     private String status;
     private Timestamp createdAt;
-    private int createdBy;
+    private int issuedCount;
 
-    public Certificate() {
+    public CertificateDTO() {
     }
 
-    public Certificate(int id, String title, int courseId, Integer categoryId, String description, String codePrefix, String status, Timestamp createdAt, int createdBy) {
+    public CertificateDTO(int id, String title, int courseId, String courseTitle, Integer categoryId, String categoryName, String description, String codePrefix, String status, Timestamp createdAt, int issuedCount) {
         this.id = id;
         this.title = title;
         this.courseId = courseId;
+        this.courseTitle = courseTitle;
         this.categoryId = categoryId;
+        this.categoryName = categoryName;
         this.description = description;
         this.codePrefix = codePrefix;
         this.status = status;
         this.createdAt = createdAt;
-        this.createdBy = createdBy;
+        this.issuedCount = issuedCount;
     }
 
     public int getId() {
@@ -61,12 +61,28 @@ public class Certificate {
         this.courseId = courseId;
     }
 
+    public String getCourseTitle() {
+        return courseTitle;
+    }
+
+    public void setCourseTitle(String courseTitle) {
+        this.courseTitle = courseTitle;
+    }
+
     public Integer getCategoryId() {
         return categoryId;
     }
 
     public void setCategoryId(Integer categoryId) {
         this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
     }
 
     public String getDescription() {
@@ -101,12 +117,12 @@ public class Certificate {
         this.createdAt = createdAt;
     }
 
-    public int getCreatedBy() {
-        return createdBy;
+    public int getIssuedCount() {
+        return issuedCount;
     }
 
-    public void setCreatedBy(int createdBy) {
-        this.createdBy = createdBy;
+    public void setIssuedCount(int issuedCount) {
+        this.issuedCount = issuedCount;
     }
 
 }
