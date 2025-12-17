2a14bae4912e9516f8acbc6f9ec5e1d0e6445834
quanndhe181858 <quanndhe181858@fpt.edu.vn>
2025-12-18 00:23:13 +0700

    Update

--- src/java/controller/trainee/TraineeCoursesController.java
+++ src/java/controller/trainee/TraineeCoursesController.java
@@ -33,12 +33,12 @@ import service.CourseServices;
     "/courses"
 })
 public class TraineeCoursesController extends HttpServlet {
-
+    
     private CourseServices _courseService;
     private CategoryServices _categoryService;
     private CourseSectionServices _courseSectionService;
     private EnrollmentDAO _enrollDao;
-
+    
     @Override
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
@@ -47,15 +47,15 @@ public class TraineeCoursesController extends HttpServlet {
         _courseSectionService = new CourseSectionServices();
         _enrollDao = new EnrollmentDAO();
     }
-
+    
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         HttpSession session = req.getSession(false);
-
+        
         if (session != null) {
             User u = (User) session.getAttribute("user");
-
+            
             if (u != null) {
                 if (u.getRole_id() == 2) {
                     resp.sendRedirect(req.getContextPath() + "/instructor/courses");
@@ -63,22 +63,22 @@ public class TraineeCoursesController extends HttpServlet {
                 }
             }
         }
-
+        
         try {
             String qs = req.getQueryString();
-
+            
             if (qs == null || qs.contains("page") || qs.contains("search")) {
                 this.getCourseCatalog(req, resp);
             } else if (qs.contains("id")) {
                 this.getCourseDetail(req, resp);
             } else {
-
+                
             }
         } catch (ServletException | IOException e) {
             resp.sendError(httpStatus.INTERNAL_SERVER_ERROR.getCode(), httpStatus.INTERNAL_SERVER_ERROR.getMessage());
         }
     }
-
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
@@ -95,10 +95,10 @@ public class TraineeCoursesController extends HttpServlet {
         try {
             String cidStr = req.getParameter("cid");
             Integer courseIdAttr = (Integer) req.getAttribute("courseId");
-
+            
             int userId = u.getId();
             int courseId = 0;
-
+            
             if (courseIdAttr != null) {
                 courseId = courseIdAttr;
             } else if (cidStr != null && !cidStr.isBlank()) {
@@ -112,9 +112,9 @@ public class TraineeCoursesController extends HttpServlet {
                 resp.sendError(httpStatus.BAD_REQUEST.getCode(), "Course ID is required");
                 return;
             }
-
+            
             boolean isEnrolled = _enrollDao.isEnrolled(userId, courseId);
-
+            
             if (isEnrolled) {
                 resp.sendRedirect(req.getContextPath() + "/learn?courseId=" + courseId);
             } else {
@@ -131,32 +131,32 @@ public class TraineeCoursesController extends HttpServlet {
                     httpStatus.INTERNAL_SERVER_ERROR.getMessage());
         }
     }
-
+    
     @Override
     protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-
+            
         } catch (Exception e) {
         }
     }
-
+    
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
-
+            
         } catch (Exception e) {
         }
     }
-
+    
     protected void getCourseCatalog(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try {
             String pageStr = req.getParameter("page");
             String search = req.getParameter("search");
             String categoryIdStr = req.getParameter("categoryId");
-
+            
             int page = paging.DEFAULT_PAGE;
             int filterCategoryId = 0;
-
+            
             if (pageStr != null && !pageStr.isBlank()) {
                 try {
                     page = Integer.parseInt(pageStr);
@@ -165,7 +165,7 @@ public class TraineeCoursesController extends HttpServlet {
                     return;
                 }
             }
-
+            
             if (categoryIdStr != null && !categoryIdStr.isBlank()) {
                 try {
                     filterCategoryId = Integer.parseInt(categoryIdStr);
@@ -174,97 +174,100 @@ public class TraineeCoursesController extends HttpServlet {
                     return;
                 }
             }
-
+            
             int offset = (page - 1) * paging.COURSE_CATALOG_ITEM_PER_PAGE;
-
+            
             List<Course> courseCatalog = _courseService.getCourseCatalog(paging.COURSE_CATALOG_ITEM_PER_PAGE, offset, search, search, filterCategoryId);
             int totalCourses = _courseService.countCourseCatalog(search, search, filterCategoryId);
-
+            
             int totalPages = (int) Math.ceil((double) totalCourses / paging.COURSE_CATALOG_ITEM_PER_PAGE);
-
+            
             int startItem = ++offset;
             int endItem = startItem + paging.COURSE_CATALOG_ITEM_PER_PAGE - 1;
-
+            
             if (endItem > totalCourses) {
                 endItem = totalCourses;
             }
-
+            
             if (totalCourses <= 0) {
                 startItem = totalCourses;
             }
-
+            
             List<Category> categories = _categoryService.getCategories();
-
+            
             List<Category> parentCategories = categories.stream()
                     .filter(c -> c.getParent_id() == 0)
                     .collect(Collectors.toList());
-
+            
             List<Category> childCategories = categories.stream()
                     .filter(c -> c.getParent_id() != 0)
                     .collect(Collectors.toList());
-
+            
             req.setAttribute("parents", parentCategories);
             req.setAttribute("children", childCategories);
-
+            
             req.setAttribute("page", page);
             req.setAttribute("courseCatalog", courseCatalog);
             req.setAttribute("totalPages", totalPages);
             req.setAttribute("startItem", startItem);
             req.setAttribute("endItem", endItem);
             req.setAttribute("totalCourses", totalCourses);
-
+            
             req.getRequestDispatcher("/View/Trainee/CourseCatalog.jsp").forward(req, resp);
         } catch (IOException e) {
             resp.sendError(httpStatus.INTERNAL_SERVER_ERROR.getCode(), httpStatus.INTERNAL_SERVER_ERROR.getMessage());
         }
     }
-
+    
     protected void getCourseDetail(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession(false);
-
+        
         try {
             String courseIdStr = req.getParameter("id");
-
+            
             if (courseIdStr == null || courseIdStr.isBlank()) {
                 resp.sendError(httpStatus.BAD_REQUEST.getCode(), httpStatus.BAD_REQUEST.getMessage());
                 return;
             }
-
+            
             int courseId = 0;
             int userId = 0;
-
+            
             if (session != null) {
                 User u = (User) session.getAttribute("user");
                 if (u != null) {
                     userId = u.getId();
                 }
             }
-
+            
             try {
                 courseId = Integer.parseInt(courseIdStr);
             } catch (NumberFormatException e) {
                 resp.sendError(400);
                 return;
             }
-
+            
             Course c = _courseService.getCourseById(courseId);
-
+            
             if (c == null || c.getUuid().isBlank()) {
                 resp.sendError(httpStatus.NOT_FOUND.getCode(), httpStatus.NOT_FOUND.getMessage());
+                return;
+            } else if (c.isHide_by_admin()) {
+                resp.sendRedirect(req.getContextPath() + "/courses");
                 return;
             } else {
                 List<CourseSection> csList = _courseSectionService.getSectionsByCourseId(c.getId());
                 boolean isEnrolled = _enrollDao.isEnrolled(userId, courseId);
-
+                
                 req.setAttribute("isEnrolled", isEnrolled);
                 req.setAttribute("course", c);
                 req.setAttribute("sections", csList);
             }
-
+            
             req.getRequestDispatcher("/View/Trainee/CourseDetail.jsp").forward(req, resp);
         } catch (IOException e) {
             resp.sendError(httpStatus.INTERNAL_SERVER_ERROR.getCode(), httpStatus.INTERNAL_SERVER_ERROR.getMessage());
         }
     }
-
+    
 }
